86898b6f0a3ab5153094dea62c5e83746c384582
Ray <cnzhuzirui@hotmail.com>
2020-12-24 11:54:12 -0500

    update newDebian.sh

--- newDebian.sh
+++ newDebian.sh
@@ -18,27 +18,51 @@ wget --no-check-certificate -O /opt/bbr.sh https://github.com/teddysun/across/ra
 chmod 755 /opt/bbr.sh
 /opt/bbr.sh
 
-dpkg-reconfigure tzdata
+# menu
+menu(){
+echo 
+}
 
-cat <<EOF > /etc/network/interfaces
-source /etc/network/interfaces.d/*
-# The loopback network interface
-auto lo
-iface lo inet loopback
-# The primary network interface
-allow-hotplug ens3
-iface ens3 inet dhcp
+# add user
+add_user(){
+    apt-get update && apt-get  -y install sudo
+    adduser ray
+    usermod -aG sudo ray
+}
+
+# config timezone
+config_timezone(){
+    dpkg-reconfigure tzdata
+}
+
+# tweak network interface
+config_newwork_interface(){
+    cat <<EOF > /etc/network/interfaces
+    source /etc/network/interfaces.d/*
+    # The loopback network interface
+    auto lo
+    iface lo inet loopback
+    # The primary network interface
+    allow-hotplug ens3
+    iface ens3 inet dhcp
 EOF
+    chattr +i /etc/network/interfaces
+}
 
-cat <<EOF > /etc/resolv.conf
-nameserver 8.8.4.4
-nameserver 1.0.0.1
+# tweak resolv
+config_resolv(){
+    cat <<EOF > /etc/resolv.conf
+    nameserver 8.8.4.4
+    nameserver 1.0.0.1
 EOF
+    chattr +i /etc/resolv.conf
+}
 
-chattr +i /etc/network/interfaces
-
-chattr +i /etc/resolv.conf
-
-cat <<EOF > /etc/hostname
-debian
+# tweak hostname
+config_hostname(){
+    cat <<EOF > /etc/hostname
+    debian
 EOF
+}
+
+
