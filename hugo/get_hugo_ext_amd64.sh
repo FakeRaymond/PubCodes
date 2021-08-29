6e92e815c64422b617629141baff1e8e4542e6ab
FakeRaymond <62877862+FakeRaymond@users.noreply.github.com>
2021-08-29 22:47:25 +0800

    Create get_hugo_ext_amd64.sh

---
+++ hugo/get_hugo_ext_amd64.sh
@@ -0,0 +1,6 @@
+newtag=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+pkg="hugo_extended_"${newtag:1}"_Linux-64bit.tar.gz"
+curl -LOs "https://github.com/gohugoio/hugo/releases/download/"$newtag"/"$pkg
+tar -xzvf $pkg
+mv hugo /usr/local/bin
+rm $pkg LICENSE	README.md
