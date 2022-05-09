0e3f2e4ec6baf617b4aef82003e08eac289cd74e
Ray <cnzhuzirui@hotmail.com>
2022-05-09 16:17:11 +0800

    update

--- ScriptsForGFW/get_v2rayN_v2ray.sh
+++ ScriptsForGFW/get_v2rayN_v2ray.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-myfun() {
-    myFile=$1
+check() {
+    target=$1
+    url=$2
 
-    if [ ! -f "$myFile" ]; then
-        touch "$myFile"
+    if [ ! -f "$target" ]; then
+        touch "$target"
+        tag_name_pre=""
+    else
+        read tag_name_pre <$target
     fi
 
-    read line <$myFile
+    tag_name_latest=$(curl -sL $url | grep -oP '"tag_name": ".*?"' | awk -F '"' '{print $4}')
 
-    latest_xray=$(curl -sL $2 | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    echo "$tag_name_latest" >$target
 
-    if [ "$line" == "$latest_xray" ]; then
+    if [ "$tag_name_pre" == "$tag_name_latest" ]; then
+        # No update
         echo 0
     else
+        # Need update
         echo 1
     fi
 
-    echo $latest_xray >$myFile
 }
 
-update1=$(myfun latest_xray https://api.github.com/repos/v2fly/v2ray-core/releases/latest)
-update2=$(myfun latest_v2rayN https://api.github.com/repos/2dust/v2rayN/releases/latest)
+update1=$(check latest_xray https://api.github.com/repos/v2fly/v2ray-core/releases/latest)
+update2=$(check latest_v2rayN https://api.github.com/repos/2dust/v2rayN/releases/latest)
 
-if [ $(($update1 + $update2)) -gt 0 ]; then
+if [[ $update1 == 1 || $update2 == 1 ]]; then
     curl -o v2ray.zip -sL https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-windows-64.zip
     curl -o v2rayN.zip -sL https://github.com/2dust/v2rayN/releases/latest/download/v2rayN.zip
     mkdir -p v2rayN
@@ -35,5 +40,5 @@ if [ $(($update1 + $update2)) -gt 0 ]; then
     zip -q -m -r v2rayN.zip v2rayN
     mv v2rayN.zip /var/www/html
 else
-    echo No update
+    echo "No update"
 fi
